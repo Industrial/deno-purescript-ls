aa33dbdb7e758a15db9a76fcaf7335a4bf1343c7
Tom Wieland <tom.wieland@gmail.com>
2023-06-25 17:25:18 +0200

    refactor(Format): move functionality to Format module

--- src/Format.purs
+++ src/Format.purs
@@ -2,23 +2,26 @@ module Format where
 
 import Prelude
 
-import Data.String (joinWith)
+import Data.Array ((!!))
+import Data.Maybe (fromMaybe)
+import Data.String (Pattern(..), joinWith, split)
+import Deno.FileSystem (DirEntry, FileInfo)
 import Permissions (FilePermissions)
 
 -- | Converts an octal integer to a Unix file permission string.
 formatPermissions :: FilePermissions -> String
 formatPermissions permissions = joinWith "" $ map permissionChar permissions
   where
-    permissionChar :: Array Boolean -> String
-    permissionChar [false, false, false] = "---"
-    permissionChar [false, false, true ] = "--x"
-    permissionChar [false, true,  false] = "-w-"
-    permissionChar [false, true,  true ] = "-wx"
-    permissionChar [true,  false, false] = "r--"
-    permissionChar [true,  false, true ] = "r-x"
-    permissionChar [true,  true,  false] = "rw-"
-    permissionChar [true,  true,  true ] = "rwx"
-    permissionChar _                     = "???"
+  permissionChar :: Array Boolean -> String
+  permissionChar [ false, false, false ] = "---"
+  permissionChar [ false, false, true ] = "--x"
+  permissionChar [ false, true, false ] = "-w-"
+  permissionChar [ false, true, true ] = "-wx"
+  permissionChar [ true, false, false ] = "r--"
+  permissionChar [ true, false, true ] = "r-x"
+  permissionChar [ true, true, false ] = "rw-"
+  permissionChar [ true, true, true ] = "rwx"
+  permissionChar _ = "???"
 
 -- | Converts a file size in bytes to a string representation.
 formatSize :: Int -> String
@@ -30,3 +33,28 @@ formatSize size
   | size < 1024 * 1024 * 1024 * 1024 * 1024 = show (size `div` (1024 * 1024 * 1024 * 1024)) <> " TiB"
   | size < 1024 * 1024 * 1024 * 1024 * 1024 * 1024 = show (size `div` (1024 * 1024 * 1024 * 1024 * 1024)) <> " PiB"
   | otherwise = show (size `div` (1024 * 1024 * 1024 * 1024 * 1024 * 1024)) <> " EiB"
+
+formatUser :: Int -> Int -> String
+formatUser gid uid = (show gid) <> "/" <> (show uid)
+
+formatName :: DirEntry -> String
+formatName entry = case entry.isDirectory of
+  true -> entry.name <> "/"
+  _ -> entry.name
+
+formatDirEntry :: DirEntry -> FileInfo -> FilePermissions -> Array String
+formatDirEntry entry stat permissions =
+  [ mode
+  , sizeAmount
+  , sizeMeasure
+  , user
+  , name
+  ]
+  where
+  name = formatName entry
+  mode = formatPermissions permissions
+  size = formatSize stat.size
+  user = formatUser stat.gid stat.uid
+  splitSize = split (Pattern " ") size
+  sizeAmount = fromMaybe "N/A" $ splitSize !! 0
+  sizeMeasure = fromMaybe "N/A" $ splitSize !! 1
--- src/Main.purs
+++ src/Main.purs
@@ -3,9 +3,8 @@ module Main where
 import Prelude
 
 import Cliffy (table)
-import Data.Array (concat, filter, (!!), sort)
-import Data.Maybe (Maybe(..), fromMaybe)
-import Data.String (Pattern(..), split)
+import Data.Array (concat, filter, sort)
+import Data.Maybe (Maybe(..))
 import Data.Traversable (traverse)
 import Deno (args)
 import Deno.FileSystem (DirEntry, readDir, stat)
@@ -13,24 +12,19 @@ import Effect (Effect)
 import Effect.Aff (Aff, launchAff_)
 import Effect.Class (liftEffect)
 import Effect.Console (log)
-import Format (formatPermissions, formatSize)
+import Format (formatDirEntry)
 import Permissions (getPermissions)
 
 getDirectory :: Effect String
 getDirectory = do
   myargs <- args
   case myargs of
-    []    -> pure "."
-    [arg] -> pure arg
-    _     -> pure "."
+    [] -> pure "."
+    [ arg ] -> pure arg
+    _ -> pure "."
 
 filePath :: String -> String -> String
 filePath directory name = directory <> "/" <> name
-
-formatEntryName :: DirEntry -> String
-formatEntryName entry = case entry.isDirectory of
-  true -> entry.name <> "/"
-  _    -> entry.name
 
 formatEntry :: String -> DirEntry -> Aff (Array String)
 formatEntry directory entry = do
@@ -38,35 +32,21 @@ formatEntry directory entry = do
   entryStat <- stat entryPath
   entryPermissions <- case getPermissions entryStat of
     Just perms -> pure perms
-    Nothing -> pure [[false, false, false], [false, false, false], [false, false, false]]
+    Nothing -> pure [ [ false, false, false ], [ false, false, false ], [ false, false, false ] ]
 
-  let name = formatEntryName entry
-  let mode = formatPermissions entryPermissions
-      size = split (Pattern " ") $ formatSize entryStat.size
-      user = (show entryStat.gid) <> "/" <> (show entryStat.uid)
-
-  let sizeAmount = fromMaybe "N/A" $ size !! 0
-  let sizeMeasure = fromMaybe "N/A" $ size !! 1
-
-  pure [
-    name,
-    mode,
-    sizeAmount,
-    sizeMeasure,
-    user
-  ]
+  pure $ formatDirEntry entry entryStat entryPermissions
 
 main :: Effect Unit
 main = launchAff_ do
   directory <- liftEffect getDirectory
   entries <- readDir directory
 
-  let directories = filter (\entry -> entry.isDirectory) entries
-  let files = filter (\entry -> not entry.isDirectory) entries
+  let directories = filter (\e -> e.isDirectory) entries
+  let files = filter (\e -> not e.isDirectory) entries
 
   directoryStats <- traverse (formatEntry directory) directories
   fileStats <- traverse (formatEntry directory) files
 
-  let entryTable = table 1 0 false (concat [sort directoryStats, sort fileStats])
+  let entryTable = table 1 0 false (concat [ sort directoryStats, sort fileStats ])
 
   liftEffect $ log entryTable
